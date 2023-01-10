d80ab94974a36a9c066daf97d60a6b8ff8a367f4
Vladimi Belov <114397508+TarjeLind@users.noreply.github.com>
2023-01-10 21:54:48 +0300

    cache test

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -3,34 +3,53 @@ package ru.netology;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static String name;
+    public static String maxName;
 
-        String maxName = null;
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("Введите имя с большой буквы:");
-            String input = scanner.nextLine();
-            if ("end".equals(input)) {
+            name = scanner.nextLine();
+
+            if ("end".equals(name)) {
                 System.out.println("Программа завершена!");
+                scanner.close();
                 break;
             }
-            String name = input;
-            char s = name.charAt(0);
-            if (Character.isUpperCase(s) && maxName == null) {
-                System.out.println("Первое имя");
-            } else if (maxName != null && Character.isUpperCase(s)) {
-                System.out.println("Введите еще");
-            } else {
-                System.out.println("Веденное имя НЕ с заглавной!");
-                continue;
-            }
-            maxName = name;
-            if (maxName != null && name.length() > maxName.length()) {
-                System.out.println("Cамое длинное имя" + maxName);
-            } else {
-                System.out.println("name");
-                System.out.println("Не хватило символов:" + (maxName.length() - name.length()));
+            boolean ret = nameSearch(name);
+            if (!ret) {
+                throw new AssertionError("Имена нужно вводить с большой буквы!");
             }
         }
+    }
+
+    public static boolean nameSearch(String inputString) {
+
+        if (!Character.isUpperCase(inputString.charAt(0))) {
+            return false;
+        }
+
+        if (maxName == null) {
+            System.out.println("Это первое введённое имя!");
+            System.out.println();
+            maxName = inputString;
+            return true;
+        }
+
+        if (inputString.length() > maxName.length()) {
+            maxName = inputString;
+            System.out.println("Самое длинное имя: " + maxName);
+            System.out.println();
+        } else if (inputString.length() == maxName.length()) {
+            System.out.println("Введённое имя равно максимальному");
+            maxName = inputString;
+            System.out.println("Самое длинное имя: " + maxName);
+        } else {
+            System.out.println("Самое длинное имя: " + maxName);
+            System.out.println("Не хватило символов: " + (maxName.length() - inputString.length()));
+            System.out.println();
+        }
+        return true;
     }
 }
--- src/test/java/ru/netology/MainTest.java
+++ src/test/java/ru/netology/MainTest.java
@@ -1,19 +1,40 @@
 package ru.netology;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class MainTest {
-
-    @BeforeEach
-    void setUp() {
+public class MainTest {
+    @Test
+    public void positiveTestFirstGreaterThenSecond(){
+        Main.nameSearch("Alexander");
+        Main.nameSearch("Dmitriy");
+        Assertions.assertDoesNotThrow(() -> {});
+        Main.name = "end";
     }
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    public void positiveTestSecondGreaterThenFirst(){
+        Main.nameSearch("David");
+        Main.nameSearch("Rachel");
+        Assertions.assertDoesNotThrow(() -> {});
+        Main.name = "end";
     }
 
+    @Test
+    public void negativeInputIntValues(){
+        Assertions.assertFalse(Main.nameSearch(String.valueOf(111)));
+        Main.name = "end";
+    }
 
+    @Test
+    public void negativeInputDoubleValues(){
+        Assertions.assertFalse(Main.nameSearch(String.valueOf(1.0D)));
+        Main.name = "end";
+    }
+
+    @Test
+    public void negativeInputStringValueNotCapital(){
+        Assertions.assertFalse(Main.nameSearch("x"));
+        Main.name = "end";
+    }
 }
